97ad421d0166f8a502072fb2a75c8af86f04cfee
realyusufismail <67903886+realyusufismail@users.noreply.github.com>
2021-02-27 19:23:36 +0000

    .

---
+++ bot.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const discord = require('discord.js');
+const client = new discord.Client();
+const PREFIX = process.env.PREFIX;
+client.login(process.env.BOT_TOKEN);
+// Set the bot's "Playing: " status (must be in an event!)
+
+client.on("ready", () => {
+})
+
+
+client.on('ready',() =>{
+ console.log(`${client.user.tag} has logged in.`);
+
+});
+
+
